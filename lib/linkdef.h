cd1705dafcd77c0ae45747f4a0582f58e6e4215c
rhodes25 <rhodes25@bugatti.llnl.gov>
2025-04-02 13:17:57 -0700

    Adding linkdef.h

---
+++ lib/linkdef.h
@@ -0,0 +1,20 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ defined_in "include/ExperimentalData.h";
+#pragma link C++ defined_in "include/Experiment.h";
+#pragma link C++ defined_in "include/GosiaMinimizerFCN.h";
+#pragma link C++ defined_in "include/GosiaMinimizer.h";
+#pragma link C++ defined_in "include/Kinematics.h";
+#pragma link C++ defined_in "include/Level.h";
+#pragma link C++ defined_in "include/Literature.h";
+#pragma link C++ defined_in "include/MatrixElement.h";
+#pragma link C++ defined_in "include/Nucleus.h";
+#pragma link C++ defined_in "include/NucleusPlotter.h";
+#pragma link C++ defined_in "include/Yield.h";
+
+#endif
